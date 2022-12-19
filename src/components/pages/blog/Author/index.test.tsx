e33bed1468ee39115f102b2b5f851df93ef16446
Rafael Almendra <rafaelalmendra28@gmail.com>
2022-12-18 23:26:11 -0300

    test: 💍 add test on author component

---
+++ src/components/pages/blog/Author/index.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import { Author } from ".";
+
+describe("Author", () => {
+  it("should render the author and date", () => {
+    render(
+      <Author
+        authorImage="
+        https://avatars.githubusercontent.com/u/67557314?v=4"
+        author="Rafael"
+        date="2021-08-01"
+      />
+    );
+
+    expect(screen.getByText("por")).toBeInTheDocument();
+    expect(screen.getByText("Rafael")).toBeInTheDocument();
+    expect(screen.getByText("2021-08-01")).toBeInTheDocument();
+  });
+
+  it("should render the author image", () => {
+    render(
+      <Author
+        authorImage="
+        https://avatars.githubusercontent.com/u/67557314?v=4"
+        author="Rafael"
+        date="2021-08-01"
+      />
+    );
+
+    expect(
+      screen.getByRole("img", {
+        name: "https://avatars.githubusercontent.com/u/67557314?v=4",
+      })
+    ).toBeInTheDocument();
+  });
+});
