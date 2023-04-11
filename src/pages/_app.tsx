4dc5c95b0f18d166bd7e178571f667f4b500bf1e
Rafael Almendra <rafaelalmendra28@gmail.com>
2023-04-11 16:11:47 -0300

    feat: change gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Script from "next/script";
 import { appWithTranslation } from "next-i18next";
 import { ToastContainer } from "react-toastify";
 import { SkeletonTheme } from "react-loading-skeleton";
@@ -24,6 +25,7 @@ import "aos/dist/aos.css";
 import GlobalStyle from "styles/global";
 import "react-toastify/dist/ReactToastify.css";
 import "react-loading-skeleton/dist/skeleton.css";
+import Head from "next/dist/shared/lib/head";
 
 const MyApp = ({ Component, pageProps }) => {
   const [theme, setTheme] = useState<ThemeProps>(verifyTheme);
@@ -37,21 +39,41 @@ const MyApp = ({ Component, pageProps }) => {
     setTheme(theme.title === "dark" ? lightTheme : darkTheme);
   };
 
+  const gtmId = process.env.GOOGLE_TAG_MANAGER_ID;
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle theme={theme} />
-      <ToastContainer />
-      <ScrollButton />
-      <Navbar handleTheme={handleTheme} />
-      <NextNProgress options={{ showSpinner: false }} />
-      <SkeletonTheme
-        baseColor={theme.title === "dark" ? "#353535" : "#e0e0e0"}
-        highlightColor={theme.title === "dark" ? "#525252" : "#f7f7f7"}
-      >
-        <Component {...pageProps} />
-      </SkeletonTheme>
-      <Footer />
-    </ThemeProvider>
+    <>
+      <Head>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', "${gtmId}");
+          `,
+          }}
+        />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        <GlobalStyle theme={theme} />
+        <ToastContainer />
+        <ScrollButton />
+        <Navbar handleTheme={handleTheme} />
+        <NextNProgress options={{ showSpinner: false }} />
+        <SkeletonTheme
+          baseColor={theme.title === "dark" ? "#353535" : "#e0e0e0"}
+          highlightColor={theme.title === "dark" ? "#525252" : "#f7f7f7"}
+        >
+          <Component {...pageProps} />
+        </SkeletonTheme>
+        <Footer />
+      </ThemeProvider>
+    </>
   );
 };
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,8 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
-import Script from "next/script";
 import React from "react";
 import { ServerStyleSheet } from "styled-components";
-
-const gtmId = process.env.GOOGLE_TAG_MANAGER_ID;
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -51,19 +48,6 @@ export default class MyDocument extends Document {
           />
           <meta charSet="utf-8" />
           <link rel="shortcut icon" href="/images/logo.png" type="png" />
-          <Script
-            id="gtm"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', "${gtmId}");
-          `,
-            }}
-          />
         </Head>
 
         <body>
